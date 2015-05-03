9ec32174dd31026f5fa2e7f88c117468c5f4ffae
Srujan Kumar Kedala <srujankk@gmail.com>
2015-05-02 20:37:49 -0400

    Updated model for dashboard Header

--- public/js/portify/views/dashboard/dashboard.js
+++ public/js/portify/views/dashboard/dashboard.js
@@ -2,8 +2,10 @@ define([
     'underscore',
     'marionette',
     'text!portify/views/dashboard/template/dashboard.html',
-    'portify/views/dashboard/dashboardList'
-], function (_, Marionette, template, DashboardList) {
+    'portify/views/dashboard/dashboardHeader',
+    'portify/views/dashboard/dashboardList',
+    'portify/data/dashboard/dashboard.collection'
+], function (_, Marionette, template, DashboardHeader, DashboardList, DashboardCollection) {
     return Marionette.LayoutView.extend({
         template: _.template(template),
         regions: {
@@ -14,7 +16,43 @@ define([
             console.log("Dashboard Initialized");
         },
         onRender: function () {
-            this.dashList.show(new DashboardList());
+            var c = new DashboardCollection(),
+                M = Backbone.Model.extend({
+                    defaults: {
+                        currentValue: 1000,
+                        investedAmount: 700
+                    }
+                }),
+
+                m = new M();
+
+            c.fetch({
+                success: function (col, response, options) {
+                    var cv = _.reduce(col.pluck('currentprice'), function (m, n) {
+                        return m + parseInt(n)
+                    }, 0);
+                    var tv = _.reduce(col.pluck('purchasePrice'), function (m, n) {
+                        return m + parseInt(n)
+                    }, 0)
+
+                    m.set({
+                        'currentValue': cv
+                    });
+
+                    m.set({
+                        'investedAmount': tv
+                    });
+
+                },
+                error: function (col, response, options) {}
+            });
+
+            this.dashHeader.show(new DashboardHeader({
+                model: m
+            }));
+            this.dashList.show(new DashboardList({
+                collection: c
+            }));
         }
     });
 });
---
+++ public/js/portify/views/dashboard/dashboardHeader.js
@@ -0,0 +1,15 @@
+define([
+    'underscore',
+    'marionette',
+    'text!portify/views/dashboard/template/dashboardHeader.html'
+], function (_, Marionette, template) {
+    return Marionette.ItemView.extend({
+        template: _.template(template),
+        initialize: function (options) {
+            this.listenTo(this.model, "change", this.render)
+        },
+        onRender: function () {
+            console.log("Rendering ......................");
+        }
+    })
+})
